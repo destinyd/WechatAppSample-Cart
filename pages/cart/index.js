1809828c444a0f5f8c7f11ee26c0fff8654e4abe
destinyd <destinyd_war@163.com>
2016-10-05 21:40:07 +0800

    计算购物车商品总额

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -6,18 +6,11 @@ var regx_plus = /btn-plus-(\d+)/
 Page({
   data: {
     cart: {},
-    goods: []
+    goods: [],
+    total: 0.0
   },
   onLoad: function () {
-    var cart = app.get_cart()
-    var goods = []
-    for(var good_id in cart){
-      goods.push(app.get_good(good_id))
-    }
-    this.setData({
-        cart: app.get_cart(),
-        goods: goods
-    })
+    this.init()
   },
   handle_click: function(e){
     var id = e.target.id
@@ -32,11 +25,21 @@ Page({
     }
   },
   handle_change_amount: function(){
+    this.init()
+  },
+  init: function(){
+    var cart = app.get_cart()
+    var goods = []
+    var total = 0.0
+    for(var good_id in cart){
+      var good = app.get_good(good_id)
+      goods.push(good)
+      total += good.price * cart[good_id]
+    }
     this.setData({
-      cart: app.get_cart()
+        cart: cart,
+        goods: goods,
+        total
     })
-    // if(amount == 0){
-    //   document.
-    // }
   }
 })
